7da25b486f7d8bb4d22566647e374c9cae952b45
knlwgl <knlwgl@gmail.com>
2023-12-27 11:02:14 +0800

    Can add/remove dice

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,33 @@
 import { useState } from "react";
 import "./App.css";
-import DiceBag from "./components/DiceBag";
 import DiceTray from "./components/DiceTray";
 import RollButton from "./components/RollButton";
 
 function App() {
   const [roll, setRoll] = useState(1);
   const [diceSize] = useState(6);
+  const [nDice, changeDice] = useState(1);
 
   return (
     <>
       <div>
-        <DiceBag></DiceBag>
-        <DiceTray sides={diceSize} n={roll} />
+        <div className="btn-group" role="group" aria-label="Basic example">
+          <button
+            type="button"
+            className="btn btn-secondary"
+            onClick={() => changeDice(nDice + 1)}
+          >
+            +
+          </button>
+          <button
+            type="button"
+            className="btn btn-secondary"
+            onClick={() => changeDice(nDice === 1 ? 1 : nDice - 1)}
+          >
+            -
+          </button>
+        </div>
+        <DiceTray sides={diceSize} n={roll} dice={nDice} />
         <RollButton
           onclick={() => {
             const n = Math.floor(Math.random() * 6) + 1;
--- src/components/DiceTray.tsx
+++ src/components/DiceTray.tsx
@@ -3,14 +3,14 @@ import Dice from "./Dice";
 interface DiceTrayProps {
   sides: number;
   n: number;
+  dice: number;
 }
 
-function DiceTray({ sides, n }: DiceTrayProps) {
-  return (
-    <h1 className="DiceTray">
-      <Dice sides={sides} roll={n}></Dice>
-    </h1>
-  );
+function DiceTray({ sides, dice }: DiceTrayProps) {
+  const diceNodes = Array.from({ length: dice }, () => {
+    return <Dice sides={sides} roll={Math.floor(Math.random() * 6) + 1}></Dice>;
+  });
+  return <h1 className="DiceTray">{diceNodes}</h1>;
 }
 
 export default DiceTray;
